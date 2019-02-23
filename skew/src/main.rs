638c71610b70dc204e09504e77c69e30df1eaf46
Vlad <vladzhukovtirko@gmail.com>
2019-02-23 23:19:08 +0300

    add skew

--- skew/src/main.rs
+++ skew/src/main.rs
@@ -1,8 +1,6 @@
 
 use std::{
     fs::File,
-    io::{BufWriter, Write},
-    collections::{HashMap, HashSet},
     io::prelude::*,
 };
 
@@ -11,10 +9,29 @@ fn main() -> std::io::Result<()> {
     let mut input = String::new();
     input_file.read_to_string(&mut input)?;
 
-    let skew = 0;
-    for i in &input {
-
+    let mut skew = 0;
+    let mut pref_skew = vec!(skew);
+    for i in input.chars() {
+        match i {
+            'C' => skew -= 1,
+            'G' => skew += 1,
+            _ => ()
+        }
+        pref_skew.push(skew);
+    };
+    let min_skew = pref_skew.iter().min().unwrap();
+    eprintln!("{:?}", min_skew);
+    let mut res = vec!();
+    for (id, value) in pref_skew.iter().enumerate() {
+        if value == min_skew {
+            res.push(id) 
+        }
+    };
+    eprintln!("hello there");
+    for i in res.iter() {
+        print!("{} ", i);
     }
+    println!();
 
     Ok(())
 }
